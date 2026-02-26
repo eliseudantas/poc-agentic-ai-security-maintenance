ca84e70861149fc4d6c9a69419b2219e093164ee
Eliseu Dantas <eliseudantasx@gmail.com>
2026-02-26 20:24:47 -0300

    Add API key constant to main.jsx

--- test-projects/nodejs-react-app/src/main.jsx
+++ test-projects/nodejs-react-app/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const APIKEY = "fakeapikey123123123123";
+
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
