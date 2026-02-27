766f1be7fef47a9c2629708dab26c548db0a91c2
Eliseu Dantas <eliseudantasx@gmail.com>
2026-02-27 11:21:15 -0300

    Upgrade dependencies: @mui/icons-material, @mui/material, @testing-library/react, @vitejs/plugin-react-swc, @vitest/coverage-v8, axios-mock-adapter, eslint, jsdom, prettier, react, react-beforeunload, react-dom, react-redux, redux, redux-actions, redux-saga, rehype-raw, vite, vite-plugin-checker, vite-plugin-svgr, vite-tsconfig-paths, vitest

--- test-projects/nodejs-react-app/vite.config.ts
+++ test-projects/nodejs-react-app/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import { esbuildCommonjs } from "@originjs/vite-plugin-commonjs";
 import eslint from "vite-plugin-eslint";
-import nodePolyfills from "vite-plugin-node-stdlib-browser";
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "node:path";
 import svgr from "vite-plugin-svgr";
@@ -11,7 +10,6 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       react(),
-      nodePolyfills(),
       ...(mode !== "production" ? [eslint()] : []),
       esbuildCommonjs(),
       svgr(),
